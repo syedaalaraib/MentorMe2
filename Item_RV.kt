cfaf7503e383e448c26d07400699ec17c797b9d2
syedaalaraib <115207236+syedaalaraib@users.noreply.github.com>
2024-03-24 21:06:54 +0500

    Add files via upload

---
+++ Item_RV.kt
@@ -0,0 +1,11 @@
+package com.laraib.i210865
+
+class Item_RV(
+    var image1: Int = R.drawable.op_grey,
+    var image2: Int = R.drawable.opaque,
+    var heart: Int = R.drawable.grey_heart,
+    var name: String? = null,
+    var description: String? = null,
+    var price: String? = null,
+    var status: String? = null
+)
